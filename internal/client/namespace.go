680fd53a7946dc5a2ba29d6de5a6e31a4f02fe51
Adalberto Junior <adalberto.gmu@gmail.com>
2025-06-21 19:58:50 -0300

    add client for fetch namespaces b context

---
+++ internal/client/namespace.go
@@ -0,0 +1,61 @@
+package client
+
+import (
+	"context"
+	"flag"
+	"path/filepath"
+	"time"
+
+	v1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/tools/clientcmd"
+	"k8s.io/client-go/util/homedir"
+)
+
+const TimeoutDuration = time.Second * 5
+
+func GetKubeConfigFile() string {
+	var kubeconfig string
+
+	if home := homedir.HomeDir(); home != "" {
+		path := filepath.Join(home, ".kube", "config")
+		flag.StringVar(&kubeconfig, "kubeconfig", path, "kubeconfig file path")
+	} else {
+		flag.StringVar(&kubeconfig, "kubeconfig", "", "kubeconfig file path")
+	}
+	flag.Parse()
+
+	return kubeconfig
+}
+
+func GetNamespacesByContext(kubeconfig, kubernetesContext string) ([]string, error) {
+	configLoader := clientcmd.NewNonInteractiveDeferredLoadingClientConfig(
+		&clientcmd.ClientConfigLoadingRules{ExplicitPath: kubeconfig},
+		&clientcmd.ConfigOverrides{CurrentContext: kubernetesContext},
+	)
+
+	config, err := configLoader.ClientConfig()
+	if err != nil {
+		return nil, err
+	}
+
+	clientSet, err := kubernetes.NewForConfig(config)
+	if err != nil {
+		return nil, err
+	}
+
+	ctx, cancel := context.WithTimeout(context.Background(), TimeoutDuration)
+	defer cancel()
+
+	list, err := clientSet.CoreV1().Namespaces().List(ctx, v1.ListOptions{})
+	if err != nil {
+		return nil, err
+	}
+
+	namespaces := make([]string, len(list.Items))
+	for i, namespace := range list.Items {
+		namespaces[i] = namespace.Name
+	}
+
+	return namespaces, nil
+}
---
+++ internal/client/namespace_test.go
@@ -0,0 +1,6 @@
+package client
+
+import "testing"
+
+func TestLoadKubeConfig(t *testing.T) {
+}
